7cde935daf2f8981374a8e19fcca68ca1521d807
Andrey Lushnikov <lushnikov@chromium.org>
2019-08-19 11:34:01 -0700

    fix: create OUTPUT dir

--- process.js
+++ process.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const util = require('util');
-const rmAsync = util.promisify(require('rimraf'));
+const fs = require('fs');
 const spawn = require('child_process').spawn;
 
 const COMPILE_COMMANDS_PATH = '/home/aslushnikov/webkit/WebKitBuild/Release/compile_commands.json';
@@ -10,8 +10,12 @@ const KYTHE_EXTRACTOR_PATH = '/opt/kythe-v0.0.30/extractors/cxx_extractor';
 
 // const COMPILE_COMMANDS_PATH = '/Users/aslushnikov/Downloads/compile_commands.json';
 
+const rmAsync = util.promisify(require('rimraf'));
+const mkdirAsync = util.promisify(fs.mkdir.bind(fs));
+
 (async() => {
   await rmAsync(KYTHE_OUTPUT_DIRECTORY);
+  await mkdirAsync(KYTHE_OUTPUT_DIRECTORY);
   const compile_commands = require(COMPILE_COMMANDS_PATH);
   const jsc_commands = compile_commands.filter(entry => entry.file.includes('JavaScriptCore'));
   console.log('JavaScriptCore compilation commands: ' + jsc_commands.length);
